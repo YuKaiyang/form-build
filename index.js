bcebb8e4de50ea53bd64524fe59567734d9affd6
zhoumiao <zhmouohz@163.com>
2017-10-11 15:20:12 +0800

    验证

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import React from 'react'
 import {render} from 'react-dom'
 import Form from "react-jsonschema-form"
 
-const myRender = (schema, uiSchema, formData, container) => {
+const myRender = (schema, uiSchema, formData, container,props) => {
 	render((
 		<Form schema={schema}
 		      uiSchema={uiSchema}
 		      formData={formData}
-		/>
+			  {...props}
+		>
+			<button type="submit">提交</button>
+		</Form>
 	), document.getElementById(container))
 }
+window.myRender = myRender.bind(this)
