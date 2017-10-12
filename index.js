ce12c042bc47f336b6cbf474eb0984968a499bd5
zhoumiao <zhmouohz@163.com>
2017-10-12 16:48:32 +0800

    errorSchema

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ import {render} from 'react-dom'
 import Form from "react-jsonschema-form"
 
 const myRender = (schema, uiSchema, formData, container, props) => {
+    if (props.errorSchema && !props.transformErrors) {
+        const transformErrors = errors => {
+            return errors.map(error => {
+                for (var key in props.errorSchema) {
+                    if (error.name === props.errorSchema[key].name && error.property === "instance." + key) {
+                        return Object.assign({}, error, {
+                            message: props.errorSchema[key].message
+                        })
+                    }
+                }
+            })
+        }
+    }
+
 	render((
 		<Form schema={schema}
 		      uiSchema={uiSchema}
